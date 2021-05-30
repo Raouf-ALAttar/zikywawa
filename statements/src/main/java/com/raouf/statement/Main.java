b6e6617df2d747810396f395dcb9d32de6434e41
ALAA CENTER <raoufalttar@gmail.com>
2021-05-30 19:36:13 +0300

    done Task7 project2

--- statements/src/main/java/com/raouf/statement/Main.java
+++ statements/src/main/java/com/raouf/statement/Main.java
@@ -1,7 +1,16 @@
 package com.raouf.statement;
 
-public class Main {
-    public static void main(String[] args) {
+import org.apache.log4j.xml.DOMConfigurator;
 
+public class Main {
+
+    static {
+        DOMConfigurator.configure("log4j.xml");
+    }
+    public static void main(String[] args) {
+        Task7 task7 = new Task7();
+//        task7.execute();
+        double radians = Math.acos(0.6);
+        System.out.println(Math.toDegrees(0.99987));
     }
 }
